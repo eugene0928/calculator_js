2cc984a3dbafe0178bb0347d0e1a87148f266475
eugene0928 <umid09tuxtayev@gmail.com>
2022-01-23 12:19:38 +0500

    move

--- move.js
+++ move.js
@@ -1,40 +1,38 @@
-let div = document.querySelector('div')
-let body = document.querySelector('body')
+let div = document.querySelector("div");
+let body = document.querySelector("body");
 
+let initialX = 0;
+let initialY = 0;
+let currentX = 0;
+let currentY = 0;
+let offsetX = 0;
+let offsetY = 0;
 
-let initialX = 0
-let initialY = 0
-let currentX = 0
-let currentY = 0
-let offsetX = 0
-let offsetY = 0
-
-let active = false
+let active = false;
 
 body.addEventListener("mousedown", (event) => {
-    initialX = event.clientX - offsetX
-    initialY = event.clientY - offsetY
+  initialX = event.clientX - offsetX;
+  initialY = event.clientY - offsetY;
 
-    if(event.target === div) {
-        active = true
-    }
-
-})
+  if (event.target === div) {
+    active = true;
+  }
+});
 
 body.addEventListener("mouseup", (event) => {
-    initialX = currentX
-    initialY = currentY
-    active = false
-})
+  initialX = currentX;
+  initialY = currentY;
+  active = false;
+});
 
 body.addEventListener("mousemove", (event) => {
-    if(active) {
-        currentX = event.clientX - initialX
-        currentY = event.clientY - initialY
+  if (active) {
+    currentX = event.clientX - initialX;
+    currentY = event.clientY - initialY;
 
-        offsetX = currentX
-        offsetY = currentY
+    offsetX = currentX;
+    offsetY = currentY;
 
-        div.style.transform = `translate3d(${currentX}px, ${currentY}px, 0)`
-    }
-})
+    div.style.transform = `translate3d(${currentX}px, ${currentY}px, 0)`;
+  }
+});
